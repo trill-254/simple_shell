6bf7f0b5cdd45235025e350fc36eee210f238eb0
june ngunjiri <mainajane316@gmail.com>
2023-11-03 04:15:49 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+
+char *read_line(void);
+int main(void);
+
+#endif /* MAIN_H */
