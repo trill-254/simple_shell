806d6a4dadf77be8f7a22ea0e36236a9b03260d9
june ngunjiri <mainajane316@gmail.com>
2023-11-16 09:24:49 -0800

    delete

---
+++ handle.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This is a simple shell script that adheres to the Betty style guidelines.
+
+function greet_user {
+    # Function to greet the user
+    echo "Hello, user!"
+}
+
+# Main function
+function main {
+    greet_user
+    echo "This script follows the Betty style guidelines."
+}
+
+# Run the main function
+main
